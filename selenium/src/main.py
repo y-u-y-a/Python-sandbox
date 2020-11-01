c6821494d714de68c78144258eee36f3aef2c129
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-01 21:33:52 +0900

    マルチプロセスで書き換え完了

--- selenium/src/main.py
+++ selenium/src/main.py
@@ -1,5 +1,6 @@
 import pprint
-import os, time, math, threading, csv
+import os, time, math, threading, csv, codecs
+from multiprocessing import Pool
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,6 +9,34 @@ import openpyxl as exl
 from modules import conversion as cv
 from modules import webdriver as wd
 from modules import process
+
+def split_list(l, n):
+    """指定した要素数の2次元配列に変換
+    l: 元リスト
+    n: サブリストの要素数
+    """
+    for idx in range(0, len(l), n):
+        yield list(l[idx:idx + n])
+
+
+def get_phone_number(corp_name) -> str:
+    """会社名から電話番号を取得"""
+
+    ch = wd.Chrome()
+    serach_word = f'{corp_name} 電話番号'
+    with ch.driver as driver:
+        # driver = ch.driver
+        driver.get('https://google.com/')
+        serach_form = driver.find_element_by_name('q')
+        serach_form.send_keys(serach_word)
+        serach_form.submit()
+        try:
+            phone_number = driver.find_element_by_class_name('mw31Ze').text
+        except Exception:
+            phone_number = ''
+        finally:
+            # driver.quit()
+            return phone_number
 
 
 def write_to_sheet(book, sheet, row, corp) -> None:
@@ -19,32 +48,10 @@ def write_to_sheet(book, sheet, row, corp) -> None:
     except Exception:
         return
 
-def get_phone_number(corp_name) -> str:
-    """会社名から電話番号の更新"""
 
-    ch = wd.Chrome()
-    driver = ch.driver
-    serach_word = f'{corp_name} 電話番号'
-    # start
-    driver.get('https://google.com/')
-    serach_form = driver.find_element_by_name('q')
-    serach_form.send_keys(serach_word)
-    serach_form.submit()
-    try:
-        phone_number = driver.find_element_by_class_name('mw31Ze').text
-    except Exception:
-        phone_number = ''
-    # end
-    finally:
-        driver.quit()
-        return phone_number
-
-# CSVへ書き込み
-def add_phone_number(w, corp_name):
-    phone_number = get_phone_number(corp_name)
-    w.writerow([corp_name, phone_number])
-    print(corp_name)
-    return
+def update_corp_dict(corp):
+    corp['phone_number'] = get_phone_number(corp['name'])
+    return corp
 
 
 if __name__ == '__main__':
@@ -56,36 +63,23 @@ if __name__ == '__main__':
     #     sheet_name='Type', # シート名を指定
     #     usecols=[1], # 使用する列を指定
     #     index_col=0)
-    def split_list(l, n):
-        """
-        リストをサブリストに分割する
-        l: リスト
-        n: サブリストの要素数
-        """
-        for idx in range(0, len(l), n):
-            yield l[idx:idx + n]
 
-    # 2. 電話番号を追加したCSVに追記
+    # 2. 会社名のみのcsvから電話番号込みのcsvを作成
     from_csv = '_storage/kaden.csv'
     to_csv = '_storage/new.csv'
-    with open(from_csv, 'r') as fr, open(to_csv, 'a') as fw:
-        r = csv.reader(fr)
-        w = csv.writer(fw)
-        # CSVをリストに変換して取得
-        rows = [row[0] for row in r]
-        sp_list = list(split_list(rows, 5))
-        # 並列処理
-        threads = []
-        for i, sub_list in enumerate(sp_list):
-            for j, corp_name in enumerate(sub_list):
-                # corp_index = i*5 + j
-                t = threading.Thread(
-                    target=add_phone_number,
-                    args=(w, corp_name))
-                t.setDaemon(True)
-                t.start()
-                threads.append(t)
-            process.wait_all_threads(threads)
+    keyname_list = ['name']
+
+    corp_list = cv.csv_to_dicts(from_csv, keyname_list)
+    sp_corp_list = split_list(corp_list, 5) # 分割
+    for sub_list in sp_corp_list:
+        # マルチプロセスで処理
+        with Pool(5) as p:
+            result_list = p.map(update_corp_dict, sub_list)
+            pprint.pprint(result_list)
+            # csv出力
+            for corp in result_list:
+                cv.add_row(to_csv, corp.values())
+
 
     # 3. シート作成・書込み
     # book = cv.create_excel_book('./_storage/result.xlsx')
